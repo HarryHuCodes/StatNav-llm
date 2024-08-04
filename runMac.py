cfdc0afb06384e057caafef44d01e6bfba232897
HarryHuCodes <48864969+HarryHuCodes@users.noreply.github.com>
2024-08-03 21:50:15 -0400

    updated requirements.txt and auto scripts to run retrieving_data and include new dependencies

--- runMac.py
+++ runMac.py
@@ -2,3 +2,4 @@ import os
 os.system('source venv/bin/activate')
 os.system('pip install -r requirements.txt')
 #run program
+os.system('python3 LLM-agent-backend/retrieving_data.py')
--- runWindows.py
+++ runWindows.py
@@ -2,3 +2,4 @@ import os
 os.system('/venv/Scripts/activate')
 os.system('pip install -r requirements.txt')
 #run program
+os.system('python LLM-agent-backend/retrieving_data.py')
